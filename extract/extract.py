caac822e1898e4f63687aac664d700120127ea57
Fernando Mavec <fermavec@gmail.com>
2022-11-30 09:39:44 -0600

    Extract

--- extract/extract.py
+++ extract/extract.py
@@ -5,6 +5,14 @@ import datetime
 
 
 def get_response(url):
+    """Take an url to get a response, then creates a soup to work with.
+
+    Args:
+        url (_str_): _web page domain_
+
+    Returns:
+        _object_: _soup created with BS4_
+    """
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
     
@@ -12,6 +20,14 @@ def get_response(url):
 
 
 def extract_links(soup):
+    """Take a soup object to get the link information from elpais.com
+
+    Args:
+        soup (_object_): _Soup created with get_response(url)_
+
+    Returns:
+        _list_: _list with all links from the given domain_
+    """
     links = soup.find('section', attrs={'class':'_g'}).find_all('article')
     links = [link.a.get('href') for link in links]
 
@@ -19,6 +35,14 @@ def extract_links(soup):
 
 
 def extract_titles(soup):
+    """Take the a soup object to get the title from elpais.com
+
+    Args:
+        soup (_object_): _Soup created with get_response(url)_
+
+    Returns:
+        _list_: _list with all titles from the given domain_
+    """
     titles = soup.find('section', attrs={'class':'_g'}).find_all('article')
     titles = [title.a.get_text() for title in titles]
 
@@ -26,6 +50,15 @@ def extract_titles(soup):
 
 
 def extract_resume(url, links):
+    """This function gets the article from link and an url in elpais.com homepage
+
+    Args:
+        url (_str_): _The domain url_
+        links (_list_): _list of links_
+
+    Returns:
+        _list_: _list of articles from elpais.com homepage_
+    """
     articles = []
     links = [link[1:] for link in links]
     
@@ -41,6 +74,14 @@ def extract_resume(url, links):
 
 
 def info_to_dict(url):
+    """Turns all the information extracted into a dictionary
+
+    Args:
+        url (_str_): _used to format link's objectn_
+
+    Returns:
+        _dict_: _All the information extracted in a dictionary_
+    """
     soup = get_response(url)
     links = extract_links(soup)
     titles = extract_titles(soup)
@@ -61,6 +102,11 @@ def info_to_dict(url):
 
 
 def run():
+    """The main function wich runs all the scripts to get data from elpais.com
+
+    Returns:
+        _file_: _csv file with all the data from elpais.com_
+    """
     URL = 'https://elpais.com/'
     data = info_to_dict(URL)
     now = datetime.datetime.now().strftime('%Y_%m_%d')
